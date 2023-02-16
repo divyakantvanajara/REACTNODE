62998b029f20ca72e9291b620632f17d1d31271b
divyakantvanajara <divyakant.1452@gmail.com>
2023-02-16 22:54:41 +0530

    bootstrap

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-var num1=10;
-var num2 =3;
-var output= <div className='container'>
+
+var output = (<div className='container'>
     <div className='row'>
-        <div className='col-12'>
-            <h1>working with bootstrap</h1>
-        </div>
-        <div className='col-12'>
-            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem beatae provident quidem magnam dolorum dicta! Dignissimos amet repudiandae facere rerum minus, magnam excepturi, harum exercitationem perferendis enim rem tempore suscipit?</h1>
+        <div className='col-6 offset-3'>
+            <div className='card shadow'>
+                <div className='card-header text-bg-primary'>
+                    <h3>Login</h3>
+                </div>
+                <div className='card-body'>
+                    <form>
+                        <div className='mb-3'>
+                            <input type='email' className='form-control' placeholder='Email' />
+                        </div>
+                        <div className='mb-3'>
+                            <input type='password' className='form-control'
+                            placeholder='Password' />
+                        </div>
+                        <div className='mb-3 text-end'>
+                            <input type='button' className='btn btn-primary' value='Login' />&nbsp;<input type='reset' className='btn btn-secondary' value='clear all' />
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
-
-</div>
-    
-
+</div>)
 root.render(output);
+
