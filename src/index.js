f5480a09b710a4223a7207eeba294662c93ef325
divyakantvanajara <divyakant.1452@gmail.com>
2023-02-16 17:29:21 +0530

    JSX

--- src/index.js
+++ src/index.js
@@ -5,17 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-var num1 = 10
-var num2 = 3
-// we can use any mathematical expression in JSX
-var output = (<div className='container'>
-        <div className='row'>
-            <div className='col-12'>
-                <h1>Working with bootstrap</h1>
-            </div>
-            <div className='col-12'>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa porro nihil accusantium sunt iure, doloremque accusamus quod! Exercitationem dolores, beatae ea explicabo eius repudiandae necessitatibus quidem temporibus voluptatibus, nesciunt cumque?</p>
-            </div>
+var num1=10;
+var num2 =3;
+var output= <div className='container'>
+    <div className='row'>
+        <div className='col-12'>
+            <h1>working with bootstrap</h1>
         </div>
-    </div>)
+        <div className='col-12'>
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem beatae provident quidem magnam dolorum dicta! Dignissimos amet repudiandae facere rerum minus, magnam excepturi, harum exercitationem perferendis enim rem tempore suscipit?</h1>
+        </div>
+    </div>
+
+</div>
+    
+
 root.render(output);
