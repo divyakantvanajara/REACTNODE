41933b90d327e63763857a0dd055fd74425ef226
divyakantvanajara <divyakant.1452@gmail.com>
2023-02-20 22:21:26 +0530

    decision making statement

--- src/index.js
+++ src/index.js
@@ -2,31 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-var now = new Date(); // Date() is libdary class in javascript 
-//create javascript object 
-var time = {
-    hour : now.getHours(),
-    minute : now.getMinutes(),
-    second : now.getSeconds()
-};
-let Greetings = function(time)
+var now = new Date();
+
+var time ={
+    hour:now.getHours(),
+    minute:now.getMinutes(),
+    second:now.getSeconds()
+}
+let Greeting= function(time)
 {
-    if (time.hour<12)
+    if(time.hour<12)
         return "Good Morning";
     else if(time.hour<=17)
-        return "Good afternoon";
-    else 
-        return "Good evening";
+        return "Good Afternoon";
+     else
+        return "Good Evening"  ; 
+}
+let page = function()
+{
+    return (
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <h1>{Greeting(time)}</h1>
+                    </div>
+                </div>
+            </div>
+    );
 }
 
-let Page = function()
-{
-    return (<div className='container'>
-        <div className='row'>
-            <div className='col-12'>
-               <h1> {Greetings(time)}</h1>
-            </div>
-        </div>
-    </div>);
-}
-root.render(Page());
+root.render(page());
