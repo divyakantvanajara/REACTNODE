51f9bbe81afcb969f8887037c97ae85b699db063
divyakantvanajara <divyakant.1452@gmail.com>
2023-03-03 11:30:37 +0530

    resturant react

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Teacher from './teacher';
 import './index.css';
+import NewTable from './NewTable';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-function Team()
+function Resturant()
 {
-    return (
-        <div className='container'>
-            <div className='row'>
-                <Teacher name="Ankit" age='38' />
-                <Teacher name="Param" age='18' />
-                <Teacher name="Dhaivat" age='25' />
-            </div>
+    return(
+    <div className='container'>
+        <div className='row'>
+        <NewTable no='1' name='Divyesh Patel' dish='2' />
+                <NewTable no='2' name='Yug Patel' dish='2' />
+                <NewTable no='3' name='Rutwik galwala' dish='4' />
+                <NewTable no='4' name='Kenil Patel' dish='3' />
+                <NewTable no='5' name='Ram Patel' dish='5' />
+                <NewTable no='6' name='Shiv Patel' dish='5' />
+                <NewTable no='7' name='Krhina Patel' dish='5' />
+                <NewTable no='8' name='Vishnu Patel' dish='5' />
         </div>
-    )
+    </div>
+    
+    );
 }
-root.render(<Team />);
+root.render(<Resturant/>);
