9d011771cd7a3f22c62942d96617a282b03c212e
divyakantvanajara <divyakant.1452@gmail.com>
2023-04-25 09:23:17 +0530

    add post

--- src/index.js
+++ src/index.js
@@ -1,47 +1,94 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-function UseEffectHookExample()
-{
-    var [posts,setPosts] = useState([]);
-    useEffect(() =>{
+function UseEffectHookExample() {
+    var [posts, setPosts] = useState([]);
+    useEffect(() => {
         var url = "https://jsonplaceholder.typicode.com/posts?_limit=5";
-        if(posts.length==0)
-            fetch(url).then((response)=> response.json()).then((data)=> setPosts(data));
+        if (posts.length == 0)
+            fetch(url).then((response) => response.json()).then((data) => setPosts(data));
     });
     var DeletePost = (id) => {
         // alert(id);
         var url = `https://jsonplaceholder.typicode.com/posts/${id}`;
-        fetch(url,{
-            method:'DELETE'
-        }).then((response)=>{
-            if(response.status === 200)
-            {
-                setPosts(posts.filter((post)=>{
+        fetch(url, {
+            method: 'DELETE'
+        }).then((response) => {
+            if (response.status === 200) {
+                setPosts(posts.filter((post) => {
                     return post.id !== id;
                 }));
             }
-            else 
+            else
                 return;
         });
     }
-    return (<div className='container'>
+    var AddPost = () => {
+        setPosts([...posts,{
+            "userId": 999,
+            "id": 199,
+            "title": "Ankit Patel Post",
+            "body": "this is post detail posted by Ankit"
+          }]);
+    }
+    return (<div className='container-fluid'>
         <div className='row'>
-            {posts.map((post)=>{
-                return <div className='col-lg-3 col-md-3 col-sm-6 col-12 border mb-3 shadow' key={post.id}>
-                <div className='card'>
-                    <div className='card-header text-bg-danger'>
-                        <h3>{post.title}</h3>
+            <div className='col-lg-3'>
+                <form>
+                    <div className='card shadow border'>
+                        <div className='card-header'>
+                            <h3>Add Post</h3>
+                        </div>
+                        <div className='card-body'>
+                            <div className='mb-3'>
+                                <label htmlFor='userid'>User Id</label>
+                                <input type='text' className='form-control' id='userid' />
+                            </div>
+                            <div className='mb-3'>
+                                <label htmlFor='title'>Title</label>
+                                <input type='text' className='form-control' id='title' />
+                            </div>
+                            <div className='mb-3'>
+                                <label htmlFor='body'>Detail</label>
+                                <textarea type='text' className='form-control' id='body'>
+                                </textarea>    
+                            </div>
+                            <div className='mb-3'>
+                                <button type='button' className='btn btn-danger' onClick={(e) => AddPost()}>Add new post</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className='card-body'>
-                    <p>
-                        {post.body}
-                    </p>
-                    <button className='btn btn-danger' onClick={()=>DeletePost(post.id)}>Delete Post</button>
+                </form>
+            </div>
+            <div className='col-lg-9'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <table className='table table-bordered table-striped' width='100%'>
+                            <thead>
+                                <tr>
+                                    <th>id</th>
+                                    <th>Title</th>
+                                    <th>Detail</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {posts.map((post) => {
+                                    return <tr key={post.id}>
+                                        <td>{post.id}</td>
+                                        <td>{post.title}</td>
+                                        <td>{post.body}</td>
+                                        <td>
+                                            <button className='btn btn-danger' onClick={() => DeletePost(post.id)}>Delete Post</button>
+                                        </td>
+                                    </tr>
+                                })}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-                </div>
-            })}
+            </div>
+
         </div>
     </div>)
 }
